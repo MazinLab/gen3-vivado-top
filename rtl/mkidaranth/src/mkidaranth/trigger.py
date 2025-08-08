4c853d324882234d21e1955bd5fa455ba67f0a84
Aled Cuda <aled@ucsb.edu>
2025-08-08 13:40:11 -0700

    Add prelim axi splitter and valve IP

--- rtl/mkidaranth/src/mkidaranth/trigger.py
+++ rtl/mkidaranth/src/mkidaranth/trigger.py
@@ -98,10 +98,40 @@ class ValvePositions(enum.Enum):
     DUMP = 0b10
     MAGIC = 0b11
 
+class StreamSplitter(wiring.Component):
+    def __init__(self, shape, count):
+        self.count = count
+        return super().__init__(
+            {
+                "input": In(stream.Signature(shape)),
+                "outputs": Out(stream.Signature(shape)).array(count),
+            }
+        )
+
+
+    def elaborate(self, platform):
+        m = Module()
+
+        complatch = Signal(self.count)
+        compcurr = Signal(self.count)
+
+        for i in range(self.count):
+            m.d.comb += self.outputs[i].payload.eq(self.input.payload)
+            with m.If(~self.complatch):
+                m.d.comb += self.outputs[i].valid.eq(self.input.valid)
+            with m.If(self.outputs[i].valid & self.outputs[i].ready):
+                m.d.sync += complatch[i].eq(1)
+                m.d.comb += compcurr.eq(1)
+        with m.If((complatch | compcurr).all()):
+            m.d.sync += complatch.eq(0)
+
+        return m
 
 class StreamValve(wiring.Component):
-    def __init__(self, shape, magic=None):
+    def __init__(self, shape, magic=None, packet=False, magic_packet_len=256):
         self.magic = magic
+        self.magic_packet_len = magic_packet_len
+        self.packet = packet
         super().__init__(
             {
                 "input": In(stream.Signature(shape)),
@@ -114,8 +144,25 @@ class StreamValve(wiring.Component):
     def elaborate(self, platform):
         m = Module()
 
-        with m.If(~self.output.valid | (self.output.valid & self.output.ready)):
-            m.d.sync += self.turning.eq(self.turn)
+        if self.packet:
+            magic_packet_counter = Signal(range(self.magic_packet_len))
+            platch = Signal(init=1)
+            pcomplete = Signal()
+            m.d.comb += pcomplete.eq(platch | (self.output.ready & self.output.valid & self.output.payload.last))
+            with m.If(pcomplete):
+                m.d.sync += platch.eq(1)
+            with m.If(self.output.valid & self.output.ready & ~self.output.payload.last):
+                m.d.sync += platch.eq(0)
+                m.d.comb += pcomplete.eq(0)
+
+        if not self.packet:
+            with m.If(~self.output.valid | (self.output.valid & self.output.ready)):
+                m.d.sync += self.turning.eq(self.turn)
+        else:
+            with m.If((~self.output.valid & pcomplete) | (self.output.valid & self.output.ready & pcomplete)):
+                m.d.sync += self.turning.eq(self.turn)
+            with m.If(~self.output.valid & self.turning == ValvePositions.CLOSED):
+                m.d.sync += self.turning.eq(self.turn)
 
         with m.If(self.turning == ValvePositions.OPEN):
             m.d.comb += [
@@ -135,7 +182,16 @@ class StreamValve(wiring.Component):
                 self.input.ready.eq(1),
                 self.output.payload.eq(self.input.payload),
             ]
+            if self.packet:
+                with m.If(self.input.valid & self.input.ready & self.input.payload.last):
+                    m.d.comb += pcomplete.eq(1)
+                    m.d.sync += platch.eq(1)
         with m.Elif(self.turning == ValvePositions.MAGIC):
+            if self.packet:
+                with m.If(self.output.valid & self.output.ready):
+                    m.d.sync += magic_packet_counter.eq(magic_packet_counter + 1)
+                    with m.If(magic_packet_counter + 1 == self.magic_packet_len):
+                        m.d.sync += magic_packet_counter.eq(0)
             m.d.comb += [
                 self.output.valid.eq(1),
                 self.input.ready.eq(0),
@@ -143,6 +199,11 @@ class StreamValve(wiring.Component):
                     self.input.payload if self.magic is None else self.magic
                 ),
             ]
+            if self.packet:
+                with m.If(magic_packet_counter + 1 == self.magic_packet_len):
+                    m.d.comb += self.output.payload.last.eq(1)
+                with m.Else():
+                    m.d.comb += self.output.payload.lase.eq(0)
         return m
 
 
